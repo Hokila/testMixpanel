6ff74e30cee2a9646c57cf4d0def9bf35ea81d06
Hokila <hokila@swag.live>
2018-04-18 19:13:47 +0800

    add other tracking

--- testMixpanel/AppDelegate.swift
+++ testMixpanel/AppDelegate.swift
@@ -23,7 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        let duration = Mixpanel.sharedInstance()?.eventElapsedTime("AppUsedTime") ?? 0.0
+        Mixpanel.sharedInstance()?.track("UserActiveDuration", properties: ["duration":duration ])
     }
+    
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -36,6 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        Mixpanel.sharedInstance()?.timeEvent("AppUsedTime")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- testMixpanel/ViewController.swift
+++ testMixpanel/ViewController.swift
@@ -13,15 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        Mixpanel.sharedInstance()?.track("EnterFirstVC", properties: nil)
+        
         let button = UIButton(type: .system)
-        button.frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
         button.setTitle("press me", for: .normal)
         button.center = self.view.center
         self.view.addSubview(button)
-        
         button.addTarget(self, action: #selector(buttonPress), for: .touchDown)
         
-        Mixpanel.sharedInstance()?.track("EnterFirstVC", properties: nil)
+        let idButton = UIButton(type: .system)
+        idButton.setTitle("update identity", for: .normal)
+        idButton.frame = button.frame.offsetBy(dx: 0, dy: 50)
+        self.view.addSubview(idButton)
+        idButton.addTarget(self, action: #selector(userStatusButtonPress), for: .touchDown)
+        
+        if let distinctId = Mixpanel.sharedInstance()?.distinctId {
+            Mixpanel.sharedInstance()?.createAlias("UserID", forDistinctID: distinctId, usePeople: true)
+        }
+        
+        //call this at login
+        Mixpanel.sharedInstance()?.identify("RealUserID", usePeople: true)
     }
 
     @objc func buttonPress() {
@@ -29,16 +42,30 @@ class ViewController: UIViewController {
         property["array"] = ["123","456"]
         property["string"] = 123456
         property["int"] = 123
-        property["float"] = Float(123.0)
-        property["double"] = Double(123.0)
+        property["float"] = Float(123.456)
+        property["double"] = Double(123.456)
         property["bool"] = true
         property["nil"] = nil
         property["NSNumberInt"] = NSNumber(value: 123)
         property["NSNumberFloat"] = NSNumber(value: Float(123.0))
         property["NSNumberDouble"] = NSNumber(value: Double(123.0))
-        property["json"] = self.loadJSON(fileName: "sample")//[String:Any]?
+        property["json"] = self.loadJSON(fileName: "sample")  //[String:Any]?
         
         Mixpanel.sharedInstance()?.track("ButtonDidPress", properties:property)
+    }
+    
+    @objc func userStatusButtonPress() {
+
+        guard let people =  Mixpanel.sharedInstance()?.people else {
+            return
+        }
+        
+        people.set("Plan", to: "Premium")
+        people.set("$name", to: "hokila")
+        people.set("$email", to: "hokila@swag.live")
+        people.set("$phone", to: "1234567")
+        
+        people.increment("Service Launch Count", by: 1)
     }
     
     func loadJSON(fileName:String,type:String = "json") -> [String:Any]? {
